ee312d3851ccb64c97ee6e2b8854a257d3a01abb
Damian <vdberg.damian@gmail.com>
2019-04-06 10:50:19 +0200

    Initial commit

---
+++ app/src/main/java/com/dvdb/starwars/ui/splash/SplashScreenActivity.kt
@@ -0,0 +1,13 @@
+package com.dvdb.starwars.ui.splash
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.dvdb.starwars.R
+
+class SplashScreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
